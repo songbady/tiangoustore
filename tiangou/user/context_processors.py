28df9ef8771a5988068b299aa5d0d546aa752e11
Isebelka <88957685+Isebelka@users.noreply.github.com>
2021-08-31 21:53:02 +0800

    Update context_processors.py

--- tiangou/user/context_processors.py
+++ tiangou/user/context_processors.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from .models import user
 
 def isLogin(request):
@@ -13,20 +12,4 @@ def isLogin(request):
         'islogin':islogin,
         'username':cur_user.username
     }
-=======
-from .models import user
-
-def isLogin(request):
-    if (request.session.get('islogin')=='1'):
-        islogin=True
-    else:
-        islogin=False
-        return {}
-    uid=request.session.get("u_id")
-    cur_user=user.objects.get(u_id=uid)
-    context={
-        'islogin':islogin,
-        'username':cur_user.username
-    }
->>>>>>> 317414a0c132534335a43ae0a7f07a23d02566ee
     return context
